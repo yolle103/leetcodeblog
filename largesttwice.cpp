fef4a1702deafcf12fde0d5cccd2d811daaba20f
liujiamei <jiamei_liu@yeah.net>
2018-08-19 02:52:36 +0800

    largest number at least twice if others

--- largesttwice.cpp
+++ largesttwice.cpp
@@ -9,22 +9,23 @@ public:
         int bigest = nums[0];
         int secondbig = nums[0];
         int index = -1;
+        //find the bigest
         for(int i=1; i<len; i++){
             if(nums[i] > bigest){
                 index = i;
-                secondbig = bigest;
                 bigest = nums[i];
             }
         }
-        cout<<secondbig<<"  "<<bigest<<endl;
-        if(secondbig*2 <= bigest)
-            return index;
-        else
-            return -1;
+        nums.erase(nums.begin()+index);
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]*2 > bigest)
+                return -1;
+            }
+        return index;
     }
                 
 };
 int main(){
-    vector<int> n = {3,6,1,0};
+    vector<int> n = {0,0,0,1};
     cout<<Solution().dominantIndex(n);
 }
