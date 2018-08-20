3af0ed34d996e42efb3ac29b970dfc0a2c64824d
liujiamei <jiamei_liu@yeah.net>
2018-08-21 03:18:06 +0800

    fix bug

--- subdomainvisit.cpp
+++ subdomainvisit.cpp
@@ -12,8 +12,12 @@ public:
         vector<string> origindomains;
         for(auto s:cpdomains){
             istringstream iss(s);
+            unordered_map<string, int>::iterator it;
             vector<string> a((istream_iterator<string>(iss)), istream_iterator<string>());
-            hash_table[a[1]] = stoi(a[0]);
+            if((it = hash_table.find(a[1]) != hash_table.end()))
+                hash_table[a[1]] += stoi(a[0]);
+            else
+                hash_table[a[1]] = stoi(a[0]);
             // extract sub domains
             string origindomain = a[1];
             istringstream sub(a[1]);
@@ -31,7 +35,6 @@ public:
                         sub_domain += ".";
                     sub_domain += tokens[k];
                 }
-                unordered_map<string, int>::iterator it;
                 if((it = hash_table.find(sub_domain)) != hash_table.end()){
                     hash_table[sub_domain] += hash_table[origindomain];
                 }
