c4d58f7a0e29cfcf1756e88c3a4d5e399e52cf13
liujiamei <jiamei_liu@yeah.net>
2018-08-22 00:07:28 +0800

    compress the functions to one o(n^2) loop

--- projectionarea.cpp
+++ projectionarea.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 class Solution {
 public:
+    int projectionArea2(vector<vector<int>> & grid){
+        int maxrow = -1;
+        int maxcolomn = -1;
+        int totalp = 0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid.size(); j++){
+                if(grid[i][j] > 0)
+                    totalp += 1;
+                maxcolomn = max(maxcolomn, grid[j][i]);
+                maxrow = max(maxrow, grid[i][j]);
+            }
+            totalp += (maxcolomn+maxrow);
+
+        }
+        return totalp;
+    }
     int projectionArea(vector<vector<int>>& grid) {
         return zproject(grid) + xproject(grid) + yproject(grid);
     }
@@ -47,5 +63,5 @@ public:
 };  
 int main(){
     vector<vector<int>> grid =  {{1,0},{0,2}};
-    cout<<Solution().projectionArea(grid)<<endl;
+    cout<<Solution().projectionArea2(grid)<<endl;
 }
