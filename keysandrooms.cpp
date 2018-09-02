7e58503f8de2197307ae605e46d7181fddb70901
liujiamei <jiamei_liu@yeah.net>
2018-09-03 00:49:16 +0800

    keys and rooms

---
+++ keysandrooms.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int roomnumber = rooms.size();
+        vector<int> visit(roomnumber, 0);
+        vector<int> temp;
+        for(auto k : rooms[0])
+            temp.push_back(k);
+
+        while(temp.size()!= 0){
+            int cur = temp.back();
+            visit[cur] = 1;
+            temp.pop_back();
+            for(auto k: rooms[cur]){
+                if(visit[k] == 0) 
+                    temp.push_back(k);
+            }
+        }
+        for(auto k: visit){
+            if(k == 0)
+                return false;
+        }
+        return true;
+    }
+
+};
+int main(){
+    // vector<vector<int>> rooms = {{1,3},{3,0,1},{2},{0}};
+    vector<vector<int>> rooms = {{1},{2},{3},{0}};
+    cout<<Solution().canVisitAllRooms(rooms);
+}
