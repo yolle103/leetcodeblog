333ca6fc28fb840e3b4547b07873422b8b646382
liujiamei <jiamei_liu@yeah.net>
2018-09-03 00:05:55 +0800

    the lenth of the string is larger than int should use long

--- decodestring.cpp
+++ decodestring.cpp
@@ -7,21 +7,19 @@ using namespace std;
 class Solution {
 public:
     string decodeAtIndex(string S, int K) {
-        int count = countlength(S);
+        long count = countlength(S);
         for(int i=S.size()-1; i>=0; i--){
             K %= count;
             if(K == 0 && isalpha(S[i]))
                 return (string)"" + S[i];
-            else{
-                if(!isdigit(S[i]))
-                    count--;
-                else
-                    count /= (int)(S[i] - '0');
-            }
+            if(!isdigit(S[i]))
+                count--;
+            else
+                count /= (int)(S[i] - '0');
         }
     }
-    int countlength(string S){
-        int count=0;
+    long countlength(string S){
+        long count=0;
         for(int i=0; i<S.size(); i++){
             if(isdigit(S[i])){
                 count *= (int)(S[i] - '0');
